845a943dcb638f5700616b7dab18f41a3c3b8af2
Gayangi Seneviratne <sgayangi@gmail.com>
2021-07-26 11:08:01 +0530

    push check

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -7,5 +7,4 @@ Map<String, Widget Function(BuildContext context)> routes = {
   SignInStart.id: (context) => SignInStart(),
   HomePage.id: (context) => HomePage(),
   CustomerHomePage.id: (context) => CustomerHomePage(),
-
 };
