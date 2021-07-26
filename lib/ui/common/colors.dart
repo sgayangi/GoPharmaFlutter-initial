da3cbc8999fdbcbf75d3397f6b8c7530bb1e80b3
Gayangi Seneviratne <sgayangi@gmail.com>
2021-07-26 11:17:55 +0530

    format

--- lib/ui/common/colors.dart
+++ lib/ui/common/colors.dart
@@ -14,19 +14,21 @@ class GoPharmaColors {
   static const LighterSecondaryColor = Color.fromRGBO(177, 61, 91, 0.1);
   static const LighterSecondaryColor2 = Color.fromRGBO(177, 61, 91, 0.7);
 
-  static const MaterialColor PrimaryMaterialColorScheme =
-      MaterialColor(0xFFB13BC8, {
-    50: Color.fromRGBO(177, 59, 200, .1),
-    100: Color.fromRGBO(177, 59, 200, .2),
-    200: Color.fromRGBO(177, 59, 200, .3),
-    300: Color.fromRGBO(177, 59, 200, .4),
-    400: Color.fromRGBO(177, 59, 200, .5),
-    500: Color.fromRGBO(177, 59, 200, .6),
-    600: Color.fromRGBO(177, 59, 200, .7),
-    700: Color.fromRGBO(177, 59, 200, .8),
-    800: Color.fromRGBO(177, 59, 200, .9),
-    900: Color.fromRGBO(177, 59, 200, 1),
-  });
+  static const MaterialColor PrimaryMaterialColorScheme = MaterialColor(
+    0xFFB13BC8,
+    {
+      50: Color.fromRGBO(177, 59, 200, .1),
+      100: Color.fromRGBO(177, 59, 200, .2),
+      200: Color.fromRGBO(177, 59, 200, .3),
+      300: Color.fromRGBO(177, 59, 200, .4),
+      400: Color.fromRGBO(177, 59, 200, .5),
+      500: Color.fromRGBO(177, 59, 200, .6),
+      600: Color.fromRGBO(177, 59, 200, .7),
+      700: Color.fromRGBO(177, 59, 200, .8),
+      800: Color.fromRGBO(177, 59, 200, .9),
+      900: Color.fromRGBO(177, 59, 200, 1),
+    },
+  );
 
   static const MaterialColor SecondaryMaterialColorScheme = MaterialColor(
     0xFFB13D5B,
@@ -45,17 +47,19 @@ class GoPharmaColors {
     },
   );
 
-  static const MaterialColor TempMaterialColorScheme =
-      MaterialColor(0xFF674EA7, {
-    50: Color.fromRGBO(103, 78, 167, .1),
-    100: Color.fromRGBO(103, 78, 167, .2),
-    200: Color.fromRGBO(103, 78, 167, .3),
-    300: Color.fromRGBO(103, 78, 167, .4),
-    400: Color.fromRGBO(103, 78, 167, .5),
-    500: Color.fromRGBO(103, 78, 167, .6),
-    600: Color.fromRGBO(103, 78, 167, .7),
-    700: Color.fromRGBO(103, 78, 167, .8),
-    800: Color.fromRGBO(103, 78, 167, .9),
-    900: Color.fromRGBO(103, 78, 167, 1),
-  });
+  static const MaterialColor TempMaterialColorScheme = MaterialColor(
+    0xFF674EA7,
+    {
+      50: Color.fromRGBO(103, 78, 167, .1),
+      100: Color.fromRGBO(103, 78, 167, .2),
+      200: Color.fromRGBO(103, 78, 167, .3),
+      300: Color.fromRGBO(103, 78, 167, .4),
+      400: Color.fromRGBO(103, 78, 167, .5),
+      500: Color.fromRGBO(103, 78, 167, .6),
+      600: Color.fromRGBO(103, 78, 167, .7),
+      700: Color.fromRGBO(103, 78, 167, .8),
+      800: Color.fromRGBO(103, 78, 167, .9),
+      900: Color.fromRGBO(103, 78, 167, 1),
+    },
+  );
 }
